a96885fe6903651727336d15dbb46592487d24e3
Vitalii Boiko <107396176+vitaliiseeker@users.noreply.github.com>
2022-07-27 14:42:32 +0300

    Написание кода ДЗ#8 ver.2

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    // body: document.querySelector("#page")
+    body: document.querySelector("#page")
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -11,6 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
-    // refs.body.classList.toggle("no-scroll");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
